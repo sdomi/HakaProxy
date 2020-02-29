33f161ce924ec3b9e169d410e6b50f1453976ab8
reds <redspl@redspl.ct8.pl>
2020-02-29 02:37:25 +0100

    small synatax fixes + sending telegram messages

--- src/telegram.js
+++ src/telegram.js
@@ -1,49 +1,46 @@
 const f = require('fastify');
 const fs = require('fs');
-
 const EventEmitter = require('events');
 
 const emitter = new EventEmitter();
-
 const app = f();
 app.register(require('fastify-formbody'));
 
-messages = []
+const messages = [];
 
 const config = JSON.parse(fs.readFileSync('../config/config.json'));
-const telegram_config = JSON.parse(fs.readFileSync('../config/telegram.json'));
+const telegramConfig = JSON.parse(fs.readFileSync('../config/telegram.json'));
 const { Client } = require('tdl');
 const { TDLib } = require('tdl-tdlib-ffi');
 
 const client = new Client(new TDLib('./libtdjson.so'), {
-	apiId: telegram_config.id, // Your api_id
-	apiHash: telegram_config.token, // Your api_hash
+	apiId: telegramConfig.id,
+	apiHash: telegramConfig.token,
 });
 
-auth = 1337;
-
 client.connectAndLogin(() => ({
-	getPhoneNumber: (retry) => (retry
-		? Promise.reject('Invalid phone number')
-		: Promise.resolve(telegram_config.user.number)),
-	getAuthCode: (retry) => new Promise((resolve, reject) => {
+	getPhoneNumber: () => new Promise((resolve, reject) => {
+		resolve(telegramConfig.user.number);
+		reject(new TypeError('Invalid phone number'));
+	}),
+	getAuthCode: () => new Promise((resolve) => {
 		emitter.once('code', resolve);
 	}),
 }));
 
-client.on('update', messageHandler);
-
 app.post('/tgAuth', async (req, res) => {
+	res.type('text/plain; charset=utf-8');
 	emitter.emit('code', req.body.auth);
 	return '';
 });
 
 app.post('/getContacts', async (req, res) => {
 	res.type('text/plain; charset=utf-8');
-	return JSON.stringify([{id:"lauraiscute",
-				name:"lauraiscute",
-				type:"telegram"
-				}]);
+	return JSON.stringify([{
+		id: 'lauraiscute',
+		name: 'lauraiscute',
+		type: 'telegram',
+	}]);
 });
 
 app.post('/getHistory', async (req, res) => {
@@ -52,24 +49,39 @@ app.post('/getHistory', async (req, res) => {
 });
 
 app.post('/sendMessage', async (req, res) => {
-	// req.body.recipient, req.body.msg
+	res.type('text/plain; charset=utf-8');
+	await client.invoke({
+		_: 'sendMessage',
+		chat_id: req.body.recipient,
+		input_message_content: {
+			_: 'inputMessageText',
+			text: {
+				_: 'formattedText',
+				text: req.body.msg,
+			},
+		},
+	});
 	return '';
 });
 
-app.listen(8888, '0.0.0.0.', (err) => {
+app.listen(config.port, config.ip, (err) => {
 	console.log(err || 'Running!');
 });
 
 function messageHandler(msg) {
-	if (msg._ == 'updateNewMessage') {
-		console.log(msg);
-		messages.push({body: msg.message.content.text.text,
-			       timestamp: msg.message.date,
-			       room: "lauraiscute",
-			       id: "lauraiscute",
-			       sender: msg.message.sender_user_id,
-			       type: "m.text"}
-);
-		console.log(msg.message.content.text);
+	if (msg._ === 'updateNewMessage') {
+		// console.log(msg.message.content.photo.sizes[1]);
+		messages.push({
+			body: msg.message.content.text.text,
+			timestamp: msg.message.date,
+			room: 'lauraiscute',
+			id: 'lauraiscute',
+			sender: msg.message.sender_user_id,
+			type: 'm.text',
+		});
+		// console.log(msg.message.content.text);
+		// console.log(msg.message.content.photo);
 	}
 }
+
+client.on('update', messageHandler);
